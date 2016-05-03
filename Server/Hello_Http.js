36aa962fd7dc7e87e65121bdad8f9522599a1024
MiSokol <so-mi-ig@yandex.ru>
2016-05-03 17:05:32 +0300

    backway from 03.05(w)

--- Server/Hello_Http.js
+++ Server/Hello_Http.js
@@ -2,11 +2,16 @@ var ejs = require('ejs-locals');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var pg = require('pg');
 var dbUrl = "postgres://postgres:q2w3e4@localhost:5432/postgres";
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'they call me mellow yellow',
+    resave: false,
+    saveUninitialized: true
+}));
 app.engine('html', ejs);
 app.engine('ejs', ejs);
 app.set('views', 'views');
@@ -55,27 +60,26 @@ var addOrder = function(order, callback) {
 
 app.get('/*', function (req, res) {
   res.render("./startPage");
-});
+}); 
 
 app.post('/main', function (req, res) {
     var code = req.body.login;
     if (code == undefined) {
-        code = 'NoPasaRun';
+        code = req.session.nameCode;
     }
-    console.log("User with code: " + req.body.login + " try to log in.");
-    
+    console.log("User with code: " + code + " try to log in.");
     getData(code, function (query) {
         var person = query.rows;
         if (person.length != 0){
-            //res.cookie('nameCode', person[0].login);
-            console.log("User with name:" + person[0].studentname + "has came in.");
+            req.session.nameCode = person[0].code;
+            console.log("User with name: " + person[0].studentname + "has came in.");
             res.render("./main", {Name: person[0].studentname, OrderForDates: "25.04 - 30.04", DeadlineOfOrder: "23.04", Price1: "1", Price2: "1", Price3: "1", Price4: "1", Price4: "1", Price5: "1",Price6: "1",Price7: "1",Price8: "1",Price9: "1", Price10: "1"});
-        }
+        } else {
+        };
     });        
 });
 
 app.post('/answer', function (req, res) {
-    console.log(req.body.s);
     res.render("./answer");
 });
 
