39617bee51801c3689a1b272e58381d172739d3b
MiSokol <so-mi-ig@yandex.ru>
2016-05-17 12:39:04 +0300

    bug fixes 17.05

--- Server/static/mainPage.js
+++ Server/static/mainPage.js
@@ -1,4 +1,8 @@
 $("li").click(function() {
-  	$( "*" ).removeClass( "active" );
-  	$( this ).addClass( "active" );
+	$( ".menu" ).removeClass( "active" );
+	$( this ).addClass( "active" );
+   	//alert(this.id);
+  
+   	$("tr").addClass("hidden");
+   	$("." + this.id + "_offer").removeClass("hidden");
 });
