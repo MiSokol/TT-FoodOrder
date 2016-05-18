82950f661f9d11691e5ca98526ea2d605479533a
MiSokol <so-mi-ig@yandex.ru>
2016-05-18 15:48:47 +0300

    bug fixed

--- Server/dbconnect.js
+++ Server/dbconnect.js
@@ -26,10 +26,54 @@ exports.dbget = function (code, callback) {
     });
 }
 
+exports.dbgetall = function (callback) {
+	pg.connect(dbUrl, function (err, client, done) {
+        var handleError = function (err) {
+            if(!err) return false;
+            done(client);
+            res.writeHead(500, {'content-type': 'text/plain'});
+            res.end('An error occurred');
+            return true;
+        };
+    
+        var qstring = "SELECT * FROM students";
+        var query = client.query(qstring);
+        var cnt = 0;
+    
+        query.on('row', function (row, result) {
+            result.addRow(row); 
+        });
+    
+        query.on('end', function(result) {
+            callback(result); 
+        });
+        done();
+    });
+};
+
 exports.dbput = function (num, code, callback) {
     pg.connect(dbUrl, function(err, client, done) {
-        var qstring = "UPDATE students SET packnum = " + num + " WHERE code = '" + code + "'"; 
-        var query = client.query(qstring);
+        var qstring = "UPDATE students SET "; 
+        var n = num - (num % 10);
+		var p = num % 10 + 1;
+		if (num.length == 1) {
+			 qstring += "monday_pack = " + p + " WHERE code = '" + code + "'; UPDATE students SET tuesday_pack = " + p + " WHERE code = '" + code + "';  UPDATE students SET wednesday_pack = " + p + " WHERE code = '" + code + "';  UPDATE students SET thursday_pack = " + p + " WHERE code = '" + code + "';  UPDATE students SET friday_pack = " + p + " WHERE code = '" + code + "';  UPDATE students SET saturday_pack = " + p + " WHERE code = '" + code + "';";
+		} else if (n == 10) {
+			qstring += "monday_pack = " + p + " WHERE code = '" + code + "';";
+		} else if (n == 20) {
+			qstring += "tuesday_pack = " + p + " WHERE code = '" + code + "';";
+		} else if (n == 30) {
+			qstring += "wednesday_pack = " + p + " WHERE code = '" + code + "';";
+		} else if (n == 40) {
+			qstring += "thursday_pack = " + p + " WHERE code = '" + code + "';";
+		} else if (n == 50) {
+			qstring += "friday_pack = " + p + " WHERE code = '" + code + "';";
+		} else if (n == 60) {
+			qstring += "saturday_pack = " + p + " WHERE code = '" + code + "';";
+		}
+		
+		
+		var query = client.query(qstring);
 		
 		query.on('row', function (row, result) {
 			result.addRow(row);
--- Server/index.js
+++ Server/index.js
@@ -34,7 +34,7 @@ app.post('/main', function (req, res) {
         if (person.length != 0){
             req.session.nameCode = person[0].code;
             console.log("User with name: " + person[0].studentname + "has came in.");
-            res.render("./main", {Name: person[0].studentname, OrderForDates: "25.04 - 30.04", DeadlineOfOrder: "23.04", Price1: "1", Price2: "1", Price3: "1", Price4: 1, Price4: "1", Price5: "1", Price6: "1", Price7: "1", Price8: "1", Price9: "1", Price10: "1"});
+            res.render("./main", {Name: person[0].studentname, OrderForDates: "23.05 - 28.05", DeadlineOfOrder: "18.05", Price1: "1", Price2: "1", Price3: "1", Price4: 1, Price4: "1", Price5: "1", Price6: "1", Price7: "1", Price8: "1", Price9: "1", Price10: "1"});
         } else {
             res.send("Wrong password. Try another one.");
 			//res.render("./startPage");
@@ -45,6 +45,8 @@ app.post('/main', function (req, res) {
 app.post('/answer', function (req, res) {
     var code = req.session.nameCode;
     var pack = req.body.choose;
+	console.log(pack - (pack % 10));
+	console.log(pack % 10);
     console.log("User with code " + code + " choose package " + pack);
     db.dbput(pack, code, function(query) {
         console.log("Order of user with code " + code + " has successfully added."); 
@@ -52,6 +54,16 @@ app.post('/answer', function (req, res) {
     res.render("./answer");
 });
 
+app.post('/history', function(req, res) {
+	var code = req.session.nameCode;
+	db.dbget(code, function (query) {
+		var person = query.rows;
+		res.render('./orderHistory', {Name: person[0].studentname, OrderForDates: "23.05 - 28.05", DeadlineOfOrder: "18.05", monday_order: person[0].monday_pack, tuesday_order: person[0].tuesday_pack, wednesday_order: person[0].wednesday_pack, thursday_order: person[0].thursday_pack, friday_order: person[0].friday_pack, saturday_order: person[0].saturday_pack});	
+	});
+	
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
